c9151136f389c580614c93855f9072b39375b7fe
Daniel <Bairindeeair@user.noreply.github.com>
2015-10-20 13:42:00 +0200

    Erster Commit

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -13,6 +15,13 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello ASE2015 - how are you? :)");
+		System.out.printf("Please enter your name: ");
+		Scanner sc= new Scanner(System.in);
+		String name=sc.next();
+		HelloUser user= new HelloUser(name);
+		user.greetUser();
+		sc.close();
+		
 	}
 
 }
